0793d0731c8e408b58296a9467faba307b66aeb7
Jesper Josefsson <jesper.josefsson@paf.com>
2025-02-27 12:14:12 +0200

    Document that removing paging makes the error go away

--- src/main/java/com/example/demo/TestTableRepository.java
+++ src/main/java/com/example/demo/TestTableRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TestTableRepository extends JpaRepository<TestTable, Long> {
     @Query("from TestTable where id = :id")
-    Page<TestTable> doQuery(Long id, Pageable pageable);
+    Page<TestTable> doesNotWork(Long id, Pageable pageable);
+
+    @Query("from TestTable where id = :id")
+    List<TestTable> works(Long id);
 }
